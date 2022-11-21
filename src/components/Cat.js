bbcf5b214fc49e7439b18ae3fda5895f81949548
Aminul Islam <aminulds61@gmail.com>
2022-11-22 00:05:47 +0600

    Side nav continue..

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material';
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <Avatar src="https://images.pexels.com/photos/667228/pexels-photo-667228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+  )
+}
+
+export default Cat;
